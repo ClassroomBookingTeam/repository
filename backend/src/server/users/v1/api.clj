8cc3f16dcd5e2e54b00140ce749251db9b9ab461
Artyom Bogachenco <artyombogachenco@gmail.com>
2024-04-08 14:00:03 +0300

    возможность(api): метод получения текущего пользователя
    закрывает #3

--- backend/src/server/users/v1/api.clj
+++ backend/src/server/users/v1/api.clj
@@ -1,8 +1,24 @@
 (ns server.users.v1.api
   (:require [buddy.hashers]
+            [clojure.spec.alpha :as s]
             [ring.util.response :as rr]
             [server.auth.authentication-cookie :as auth-cookie]
+            [server.spec :as spec]
             [server.users.v1.db :as db.users]))
+
+(defn get-current-user
+  [ctx user-id]
+  (let [ds (:pg-ds ctx)
+        user (db.users/get-by-id ds user-id)]
+    (if user
+      (-> (rr/response user)
+          (rr/status 200))
+      (rr/not-found []))))
+
+(s/def #sdkw :user/password string?)
+(s/def :session-check-user-data/params
+  (s/keys :req-un [#sdkw :user/password
+                   ::spec/email]))
 
 (defn session-check-user-data
   [ctx request]
